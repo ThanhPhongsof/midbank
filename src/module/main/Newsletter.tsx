1b51d73075bb00f69c439a53b058bd975322a8e3
ThanhPhongsof <ttphongletter@gmail.com>
2022-08-19 15:21:48 +0700

    feat(newsletter) : add newsletter

--- src/module/main/Newsletter.tsx
+++ src/module/main/Newsletter.tsx
@@ -1,9 +1,34 @@
+import { Button } from "components/button";
+import { Desc, Heading } from "components/common";
 import { LayoutContainer } from "components/layout";
 
 const Newsletter = () => {
   return (
-    <section className="newsletter">
-      <LayoutContainer>Newsletter</LayoutContainer>
+    <section className="newslette py-[70px]">
+      <LayoutContainer>
+        <div className="flex flex-col items-center text-center rounded-[20px] newsletter-background py-12">
+          <Heading className="mb-4">Subscribe Newsletter</Heading>
+          <Desc className="mb-11" maxWidth="w-full md:max-w-[420px]">
+            We operate our banking services in many countries around the world.
+          </Desc>
+          <form autoComplete="off">
+            <div className="flex flex-col md:flex-row items-center gap-4">
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="py-6 pl-9 placeholder:text-white placeholder:text-opacity-50 text-white border-2 border-primary50 border-solid bg-transparent rounded-full w-[332px]"
+              />
+              <Button
+                type="submit"
+                kind="primary"
+                className="text-base py-6 px-9 w-full md:max-w-max"
+              >
+                Get Started
+              </Button>
+            </div>
+          </form>
+        </div>
+      </LayoutContainer>
     </section>
   );
 };
--- src/module/main/Testimonial.tsx
+++ src/module/main/Testimonial.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Testimonial = () => {
   return (
-    <section id="blog" className="testimonial">
+    <section id="blog" className="testimonial h-[710px]">
       <LayoutContainer>Testimonial</LayoutContainer>
     </section>
   );
